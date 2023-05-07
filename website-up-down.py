2528a8e40584f88c70d7424869fa441178864865
Jonatas Silva <jonatas1200@icloud.com>
2023-05-06 23:01:11 -0300

    Função enviar e-mail criada

--- website-up-down.py
+++ website-up-down.py
@@ -1,5 +1,26 @@
 import urllib.request
 import urllib.error
+import smtplib
+
+
+def env_email():
+    remetente = ''
+    senha = ''
+    destinatario = ''
+
+    assunto = ''
+    corpo = ''
+
+    mensagem = f'Subject: {assunto}\n\n{corpo}'
+
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as servidor:
+            servidor.starttls()
+            servidor.login(remetente, senha)
+            servidor.sendmail(remetente, destinatario, mensagem)
+            print('E-mail enviado com sucesso!')
+    except Exception as e:
+        print(f'Erro ao enviar o e-mail: {e}')
 
 
 try:
